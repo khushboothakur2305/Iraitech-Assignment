2904cd23894a0216b01d7d43d03c83e3473f27f3
khushboothakur2305 <khushboothakur2305@gmail.com>
2020-12-15 22:02:36 +0530

    updateupdate

--- src/app/Components/second-question/second-question.component.ts
+++ src/app/Components/second-question/second-question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './Service/service.service';
 
 @Component({
   selector: 'app-second-question',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecondQuestionComponent implements OnInit {
   userComponets = [];
-  constructor() {}
+  count = -1;
+  constructor(private service: ServiceService) {}
 
   ngOnInit(): void {}
   addComponents() {
+    this.count = this.count + 1;
+    console.log(this.count);
+    this.service.getValue(this.count);
     this.userComponets.push('HEllo');
   }
 }
--- src/app/Components/second-question/xcomponent/xcomponent.component.ts
+++ src/app/Components/second-question/xcomponent/xcomponent.component.ts
@@ -1,18 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ServiceService } from '../Service/service.service';
 @Component({
   selector: 'app-xcomponent',
   templateUrl: './xcomponent.component.html',
   styleUrls: ['./xcomponent.component.scss'],
 })
 export class XComponentComponent implements OnInit {
-  constructor(private service: ServiceService) {}
+  constructor() {}
   @Input() componentCount;
   ycomponents = [];
   ngOnInit(): void {}
   addYComponents() {
     console.log(this.componentCount);
-    this.service.getValue(this.componentCount);
     this.ycomponents.push('Hello');
   }
 }
